b6704105048694eda8762f943ccaaa08aa07903a
Victor Sanjines <victorsanjines667@gmail.com>
2022-09-28 16:30:17 -0400

    configuracion de modo avion con broadcast

--- app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.ejemplobroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejemplobroadcast.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val getAirplaneMode = object: BroadcastReceiver(){
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //puede ser true or false
+            //true si esta habilitado el modo avion
+            val airplaneMode = intent?.getBooleanExtra("state", false)
+            airplaneMode?.let {
+                val mensaje = if (it) "Modo Avión Activado" else "Modo Avión Desactivado"
+                binding.txtModoAvion.text = mensaje
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerReceiver(getAirplaneMode, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(getAirplaneMode)
     }
 }
+
+
+
+
+
+
+
+
+
