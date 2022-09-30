fbc3a1c8ce67f3d4d9464fe867d985df2db76a44
Victor Sanjines <victorsanjines667@gmail.com>
2022-09-30 15:51:32 -0400

    servicios de wifi y cambio de tiempo

--- app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejemplobroadcast.databinding.ActivityMainBinding
@@ -21,6 +22,30 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private val getTimeChange = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            val tiempo = "El tiempo ha cambiado en el celular"
+            binding.txtTiempo.text = tiempo
+        }
+    }
+
+    private val getWifiMode = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //EXTRA_WIFI_STATE = llave del registro entero que te informa los estados del wifi
+            val wifiMode = intent?.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN)
+            wifiMode?.let {
+                binding.txtWifi.text = when(it) {
+                    WifiManager.WIFI_STATE_ENABLED -> "Wifi habilitado"
+                    WifiManager.WIFI_STATE_DISABLED -> "Wifi deshabilitado"
+                    WifiManager.WIFI_STATE_UNKNOWN -> "Servicio no reconocido"
+                    else -> "Posible error en el servicio Wifi"
+                }
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -30,11 +55,15 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         registerReceiver(getAirplaneMode, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+        registerReceiver(getTimeChange, IntentFilter(Intent.ACTION_TIME_TICK))
+        registerReceiver(getWifiMode, IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION))
     }
 
     override fun onStop() {
         super.onStop()
         unregisterReceiver(getAirplaneMode)
+        unregisterReceiver(getTimeChange)
+        unregisterReceiver(getWifiMode)
     }
 }
 
