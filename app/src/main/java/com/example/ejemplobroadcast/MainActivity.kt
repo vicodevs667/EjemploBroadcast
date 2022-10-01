28ac6075b2d3c6628707bb582ef7cffd0283addf
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-01 10:51:14 -0400

    modo time tick, modo wi fi

--- app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.net.wifi.WifiManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ejemplobroadcast.databinding.ActivityMainBinding
@@ -26,16 +27,44 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+    // Configurar un Broadcast Receiver para el evento de cambio de tiempo
+    // a este se le denomina Time Tick
+    private val getTimeChange = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            binding.txtTimeTick.text = "El tiempo ha cambiado"
+        }
     }
+    //Vamos a configurar el BroadcastReceiver
+    //para saber cuando el Wifi esta habilitado o no
+    private val getWifiMode = object: BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            //Wifi trabaja pasando a traves de un registro en el
+            //Intent un dato de tipo numérico, ese dato
+            //te va a indicar aspectos como si esta habilitado,
+            //si no esta habilitado, configuraciones de la red,
+            //si el servicio esta dañado o no se reconoce
+            //Servicio Wifi: se gestiona a través de un controlador
+            //que es una clase que lo administra -> WifiManager
+            val wifiMode = intent?.getIntExtra(WifiManager.EXTRA_WIFI_STATE, WifiManager.WIFI_STATE_UNKNOWN)
+            wifiMode?.let {
+                val message = when (it) {
+                   WifiManager.WIFI_STATE_ENABLED -> "WiFi habilitado"
+                   WifiManager.WIFI_STATE_DISABLED -> "Wifi esta deshabilitado"
+                   WifiManager.WIFI_STATE_UNKNOWN -> "Servicio Wifi error desconocido"
+                   else -> "No dispones de WiFi, actualizate"
+                }
+                binding.txtModoWifi.text = message
+            }
+        }
+    }
+
 
     override fun onStart() {
         super.onStart()
         //Registrar el BroadcastReceiver
         registerReceiver(getAirplaneMode, IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED))
+        registerReceiver(getTimeChange, IntentFilter(Intent.ACTION_TIME_TICK))
+        registerReceiver(getWifiMode, IntentFilter(WifiManager.WIFI_STATE_CHANGED_ACTION))
     }
 
 
@@ -43,6 +72,7 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         //Quitar el registro
         unregisterReceiver(getAirplaneMode)
+        unregisterReceiver(getTimeChange)
     }
 }
 
