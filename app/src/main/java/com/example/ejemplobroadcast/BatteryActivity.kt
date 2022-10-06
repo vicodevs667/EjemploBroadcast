a1e355d630e54cfa79aeeef7c83472b60bacb452
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-06 09:09:30 -0400

    configuracion broadcast nivel bateria

--- app/src/main/java/com/example/ejemplobroadcast/BatteryActivity.kt
+++ app/src/main/java/com/example/ejemplobroadcast/BatteryActivity.kt
@@ -1,11 +1,45 @@
 package com.example.ejemplobroadcast
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.ejemplobroadcast.databinding.ActivityBatteryBinding
 
 class BatteryActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBatteryBinding
+    //Necesitamos crear un objeto de la clase BroadcastReceiver
+    private lateinit var myBroadcast: MyBroadcast
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_battery)
+        binding = ActivityBatteryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //Inicializar una instancia de la clase broadcast receiver
+        myBroadcast = MyBroadcast(binding)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        //Registrar su broadcast receiver
+        registerReceiver(myBroadcast, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        //Crear un intent, configurarlo, e intentar resolverlo
+        val intent = Intent(this, MyBroadcast::class.java)
+        sendBroadcast(intent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //Quitar el registro de ese broadcastreceiver
+        unregisterReceiver(myBroadcast)
     }
 }
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
@@ -3,14 +3,34 @@ package com.example.ejemplobroadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.BatteryManager
+import com.example.ejemplobroadcast.databinding.ActivityBatteryBinding
 
 //Para que esta clase se comporte como un
 //BroadcastReceiver debes extender o heredar
 //una clase abstracta que ustedes ya han utilizado
 //3 veces en el ejercicio anterior
-class MyBroadcast: BroadcastReceiver() {
+class MyBroadcast(
+    private val bindingObject: ActivityBatteryBinding
+): BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Not yet implemented")
+        when(intent?.action) {
+            Intent.ACTION_BATTERY_CHANGED -> showBatteryLevel(intent)
+        }
+    }
+
+    private fun showBatteryLevel(intent: Intent?) {
+        //Cuando se trata del nivel de la bateria, el servicio del sistema
+        //les envia a través de un intent un valor entero que representa
+        //el porcentaje de bateria restante
+        //El tema de la bateria generalmente es gestionado y configurado
+        //en un clase que controla todos estos aspectos y se llama BatteryManager
+        val batteryLevel = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+        batteryLevel?.let {
+            val porcentaje = "$it% bateria"
+            bindingObject.txtPorcentajeBateria.text = porcentaje
+            bindingObject.pbNivelBateria.progress = it
+        }
     }
 }
 
