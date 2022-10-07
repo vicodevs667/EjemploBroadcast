4a7aebc6d135d65bb5b2dae772451452b2451ee2
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-07 18:24:05 -0400

    configuracion permisos brillo

--- app/src/main/java/com/example/ejemplobroadcast/BateriaActivity.kt
+++ app/src/main/java/com/example/ejemplobroadcast/BateriaActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import com.example.ejemplobroadcast.databinding.ActivityBateriaBinding
 
 class BateriaActivity : AppCompatActivity() {
@@ -16,11 +17,16 @@ class BateriaActivity : AppCompatActivity() {
         setContentView(binding.root)
         //inicializar una instancia de su broadcast receiver
         myBroadcast = MyBroadcast(binding)
+        //Vamos a configurar el click del FAB
+        binding.fabPermission.setOnClickListener {
+            enableSettingsPermission()
+        }
     }
     //Registrar el broadcast receiver
     override fun onStart() {
         super.onStart()
         registerReceiver(myBroadcast, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        registerReceiver(myBroadcast, IntentFilter(Intent.ACTION_BATTERY_LOW))
         val intent = Intent(this, myBroadcast::class.java)
         sendBroadcast(intent)
     }
@@ -29,6 +35,14 @@ class BateriaActivity : AppCompatActivity() {
     override fun onStop() {
         super.onStop()
         unregisterReceiver(myBroadcast)
+    }
+
+    private fun enableSettingsPermission() {
+        //a través de un intent
+        //estan lanzando una pantalla del sistema android
+        //en este caso la pantalla de settings de las apps
+        val intent = Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS)
+        startActivity(intent)
     }
 }
 
--- app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.BatteryManager
+import android.os.Build
+import android.provider.Settings
+import androidx.annotation.RequiresApi
 import com.example.ejemplobroadcast.databinding.ActivityBateriaBinding
 
 //Para que esta clase se comporte como
@@ -14,18 +17,47 @@ import com.example.ejemplobroadcast.databinding.ActivityBateriaBinding
 class MyBroadcast(
     val bindingObject: ActivityBateriaBinding
 ): BroadcastReceiver() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context?, intent: Intent?) {
         when(intent?.action) {
             Intent.ACTION_BATTERY_CHANGED -> showBatteryStatus(intent)
-            Intent.ACTION_BATTERY_LOW -> showAlertBatteryLow(intent)
+            Intent.ACTION_BATTERY_LOW -> showAlertBatteryLow(context, intent)
         }
     }
 
-    private fun showAlertBatteryLow(intent: Intent?) {
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showAlertBatteryLow(context: Context?, intent: Intent?) {
         //La bateria baja es tratada a partir de un
         //estado booleano true cuando esta baja
         //false cuando esta normal en carga
         val batteryLevel = intent?.getBooleanExtra(BatteryManager.EXTRA_BATTERY_LOW, false)
+        batteryLevel?.let {
+            bindingObject.txtMensajeBateria.text = "Bateria baja"
+            changeScreenBrightness(context)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun changeScreenBrightness(context: Context?) {
+        //El brillo esta definido en un rango de 0 a 255
+        //donde 255 es el brillo máximo que puede haber
+        val screenBrightness = 15
+        //No puedes alterar un servicio del sistema
+        //como son los settings de tu celular
+        //a libre gusto si no tienes los permisos habilitados
+        val brightnessPermission = hasWriteSettingsPermission(context)
+        if (brightnessPermission) {
+            TODO("Implementar cambio de brillo")
+        } else {
+            TODO("Implementar alterta cuando no tienes permiso")
+        }
+    }
+
+    //Este metodo lo único que hace es validar si tienes
+    //o no tienes permisos
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun hasWriteSettingsPermission(context: Context?): Boolean {
+        return Settings.System.canWrite(context)
     }
 
     private fun showBatteryStatus(intent: Intent?) {
