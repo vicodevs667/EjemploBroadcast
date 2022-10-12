c71ab11402f8a01a4dcb56d006d47dd6a9a747af
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-12 13:14:25 -0400

    ajustes de bateria baja

--- app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
@@ -4,21 +4,42 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.BatteryManager
+import com.example.ejemplobroadcast.databinding.ActivityBatteryBinding
 
 //Para que esta clase se comporte como un
 //Broadcast Receiver hay que extender
 //una clase abstracta que ustedes ya la
 //han usado 3 veces en el ejemplo BroadcastReceiver()
-class MyBroadcast: BroadcastReceiver() {
+class MyBroadcast(
+    private val bindingObject: ActivityBatteryBinding
+): BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("Configurar nivel de bateria")
         when(intent?.action) {
             Intent.ACTION_BATTERY_CHANGED -> showBatteryLevel(intent)
+            Intent.ACTION_BATTERY_LOW -> configureBatteryLow(intent)
         }
     }
 
-    private fun showBatteryLevel(intent: Intent) {
+    private fun configureBatteryLow(intent: Intent?) {
+        //El dato que les llega a través del archivo Extras del Intent
+        //es un booleano
+        val batteryLow = intent?.getBooleanExtra(BatteryManager.EXTRA_BATTERY_LOW, false)
+        batteryLow?.let {
+            bindingObject.txtMensajeBateria.text = "Alerta Bateria Baja"
+        }
 
+    }
+
+    private fun showBatteryLevel(intent: Intent?) {
+        //un valor entero que te llega en el Extras del Intent
+        //que el valor del nivel de la bateria
+        //La bateria tiene un gestor Llamado BatteryManager
+        val batteryLevel = intent?.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
+        batteryLevel?.let {
+            val message = "Porcentaje bateria $it%"
+            bindingObject.txtPorcentajeBateria.text = message
+            bindingObject.pbNivelBateria.progress = it
+        }
     }
 }
 
