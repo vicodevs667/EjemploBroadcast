39eddfffedce552ecd79d9dfeaa246dc46cb981f
Victor Sanjines <victorsanjines667@gmail.com>
2022-10-13 10:02:55 -0400

    validacion de uso de brillo de pantalla

--- app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
+++ app/src/main/java/com/example/ejemplobroadcast/MyBroadcast.kt
@@ -4,7 +4,12 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.BatteryManager
+import android.os.Build
+import android.provider.Settings
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import com.example.ejemplobroadcast.databinding.ActivityBatteryBinding
+import kotlin.math.round
 
 //Para que esta clase se comporte como un
 //BroadcastReceiver debes extender o heredar
@@ -13,6 +18,8 @@ import com.example.ejemplobroadcast.databinding.ActivityBatteryBinding
 class MyBroadcast(
     private val bindingObject: ActivityBatteryBinding
 ): BroadcastReceiver() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onReceive(context: Context?, intent: Intent?) {
         when(intent?.action) {
             Intent.ACTION_BATTERY_CHANGED -> showBatteryLevel(intent)
@@ -20,6 +27,7 @@ class MyBroadcast(
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun evaluateLowBattery(context: Context?, intent: Intent?) {
         //El intent que resuelve el tema de la bateria baja
         //maneja un dato en su registro temporal de tipo booleano
@@ -30,8 +38,28 @@ class MyBroadcast(
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     private fun configureScreenBrightness(context: Context?) {
-
+        //No pueden usar recursos de hardware y configuraciones del sistema
+        //a libre gusto, si no hay permisos o no estan habilitadas las opciones
+        //no pueden ejecutar esos métodos
+        if (hasWriteSettingsEnabled(context)) {
+            //1: el nivel de brillo se maneja de 0 a 255
+            //2: por defecto el brillo se ajusta automáticamente, por ende primero hay que cambiar a modo manual
+            val screenBrightnessLevel = 20
+            Settings.System.putInt(
+                context?.contentResolver,
+                Settings.System.SCREEN_BRIGHTNESS_MODE,
+                Settings.System.SCREEN_BRIGHTNESS_MODE_MANUAL
+            )
+            Settings.System.putInt(
+                context?.contentResolver,
+                Settings.System.SCREEN_BRIGHTNESS,
+                screenBrightnessLevel
+            )
+            val percentage = screenBrightnessLevel.toDouble() / 255
+            Toast.makeText(context, "Nivel ${round(percentage * 100)}%", Toast.LENGTH_SHORT).show()
+        } else Toast.makeText(context, "No puedes configurar los settings", Toast.LENGTH_SHORT).show()
     }
 
     private fun showBatteryLevel(intent: Intent?) {
@@ -47,7 +75,27 @@ class MyBroadcast(
             bindingObject.pbNivelBateria.progress = it
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun hasWriteSettingsEnabled(context: Context?): Boolean {
+        return Settings.System.canWrite(context)
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
